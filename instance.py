66439d1eb5ac1a509d42afacb6c9415d022f190b
Alex Todicescu <alex.todicescu@gmail.com>
2018-02-19 00:40:55 +0000

    working

--- instance.py
+++ instance.py
@@ -7,8 +7,10 @@
 * Description: Helper script to stop/start EC2 instances based on Environment tag
 * Usage:
 * 
-* Notes: (TODO) DescribeTags is paginated with max 1000 results. If number of instance ids returned > 1000, need to do additional DescribeTags operations.
+* Notes:    (TODO) Add arguments validation in argparse section. Only allow start|stop operations
+*           (TODO) DescribeTags is paginated with max 1000 results. If number of instance ids returned > 1000, need to do additional DescribeTags operations.
 * https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_DescribeTags.html
+*           (TODO) Maybe switch to using a single StopInstances/StartInstances for all instanceIds instead of one for each instanceId. Any throttles on StopIntanceIds?
 *
 */
 '''
@@ -21,7 +23,7 @@ logging.basicConfig(level=logging.INFO)
 
 # Read in command-line parameters
 parser = argparse.ArgumentParser()
-parser.add_argument("-o", "--operation", action="store", required=True, dest="operation", help="EC2 oepration to be perfromed.")
+parser.add_argument("-o", "--operation", action="store", required=True, dest="operation", help="EC2 operation to be perfromed.")
 parser.add_argument("-e", "--environment", action="store", required=True, dest="environment", help="Value of the Environment tag on which the operation will be performed")
 parser.add_argument("-r", "--region", action="store", required=False, dest="region", default="eu-west-1", help="Region where the operation will be performed. Default: eu-west-1")
 
@@ -40,4 +42,23 @@ response = ec2.describe_tags(
     MaxResults=1000, 
 #    NextToken='string'
 )
-logging.info (response)
+# logging.info(response)
+
+tags = response["Tags"]
+for tag in tags:
+    instanceid = tag["ResourceId"] 
+    logging.info("Attempting to " + operation + " instanceId " + instanceid)
+    
+    if operation == "start":
+        response = ec2.start_instances(InstanceIds=[instanceid])        
+    elif operation == "stop":
+        response = ec2.stop_instances(InstanceIds=[instanceid])
+    else:
+        logging.error("Incorrect operation type selected.")
+
+    # Check if operation was successful    
+    if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+        logging.info("Async operation on instanceId " + instanceid + " was successful.")
+    else:
+        logging.info("Async operation on instanceId " + instanceid + " failed.")
+ 
